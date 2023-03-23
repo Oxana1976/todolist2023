eff5a7a7d7ba3b554b9f6cef17c641f82e2b53d9
Bob Sull <0101aohamouch@student.epfc.eu>
2023-03-23 12:21:25 +0100

    Version0.1 fonctionnel

--- js/app.js
+++ js/app.js
@@ -1,4 +1,18 @@
 //selection
 const inputTask = document.forms["frmTODO"].task;
 const btAdd = document.forms["frmTODO"].btAdd;
-const taskList = document.getElementById("taskList")
+const taskList = document.getElementById("taskList");
+const divNotification = documnet.getElementById("notification");
+
+btAdd.onclick = function(){
+	let task = inputTask.value;
+	if(task!='' && task.length>=3){
+		divNotification.innerHTML = '';
+		
+		taskList.innerHTML +='<li>'+task+'</li>'
+	} else {
+		divNotification.innerHTML = 'Veuillez entre une tâche d\'au mois 3 car'
+		
+	}
+	
+}
